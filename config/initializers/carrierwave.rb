c1b1dfa1dd243ac12fd71f02704bb5e197d12d80
Thomasin Durgin <td@thomasin.com>
2019-10-16 22:26:46 +0000

    fixing errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,22 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  #config.storage    = :aws
+  config.fog_directory = ENV["AWS_BUCKET"]
+  #config.aws_acl    = "public-read"
+
+
+  
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],                        # required unless using use_iam_profile
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    region:                ENV["AWS_REGION"]                        # required unless using use_iam_profile
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+
 end
