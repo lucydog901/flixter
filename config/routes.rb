8a6d7cca0bcbe0f68304d68f3012d5ee4927bdfd
Thomasin Durgin <td@thomasin.com>
2019-11-05 06:08:41 +0000

    lesson 36

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
